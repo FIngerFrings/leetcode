ac95ee2ba009ed6ba8763ceb4ecded4ec10a6983
Finger Frings <67583493+FIngerFrings@users.noreply.github.com>
2021-03-29 22:21:39 +0800

    Update leetcode20.cpp

--- Easy/leetcode20.cpp
+++ Easy/leetcode20.cpp
@@ -33,3 +33,30 @@ public:
         return stk.empty();
     }
 };
+
+//方法二：使用栈，原理是先算出nums2中每个元素的下一个最大值，将其存放在哈希表中，之后在哈希表中查找nums1中每个元素的下一个最大值，这里对nums2使用了前序遍历
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        unordered_map<int, int> q;
+        vector<int> p;
+        stack<int> stk;
+        for(int i = 0; i < nums2.size(); i++){
+            while(!stk.empty() && stk.top() < nums2[i]){
+                //q.insert(pair<int, int>(stk.top(), nums2[i]));
+                q[stk.top()] = nums2[i];
+                stk.pop();
+            }
+            stk.push(nums2[i]);
+        }
+        for(int i = 0; i < nums1.size(); i++){
+            if(q.count(nums1[i])){
+                p.push_back(q[nums1[i]]);
+            }
+            else{
+                p.push_back(-1);
+            }
+        }
+        return p;
+    }
+};
