d7a835e6365b03b0e9023532e532e322c7ff43f9
Finger Frings <67583493+FIngerFrings@users.noreply.github.com>
2021-03-31 21:47:12 +0800

    Update leetcode496.cpp

--- Easy/leetcode496.cpp
+++ Easy/leetcode496.cpp
@@ -26,4 +26,51 @@ public:
     }
 };
 
-//方法二：思路为
+//方法二：思路为先将nums2中每个数的下一个最大元素算出，然后放进哈希表中，之后遍历nums1，得到nums1中每个值对应的下一个最大值
+//该方法对nums2执行先序遍历，如果栈为空，说明是第一个元素，则压入栈中，否则将该元素与栈顶元素进行比较，如果大则将栈顶元素取出并存放进哈希表中，否则将当前元素压入栈中
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        unordered_map<int, int> hx;
+        stack<int> stk;
+        vector<int> p;
+        for(int i = 0; i < nums2.size(); i++){
+            while(!stk.empty() && nums2[i] > stk.top()){
+                hx[stk.top()] = nums2[i];
+                stk.pop();
+            }
+            stk.push(nums2[i]);
+        }
+        while(!stk.empty()){
+            hx[stk.top()] = -1;
+            stk.pop();
+        }
+        for(int i = 0; i < nums1.size(); i++){
+            p.push_back(hx[nums1[i]]);
+        }
+        return p;
+    }
+};
+
+//方法三是对方法二的变形，这里对nums2进行后序遍历
+//for循环中的while循环实际上就是让栈顶元素为当前元素的下一个最大值，如果为空，说明当前元素后面没有比它大的数了，之后利用三元运算符，得到当前元素的下一个最大值，如果栈为空，说明后面没有比它大的值，则返回-1，如果不为空，则返回栈顶元素
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        unordered_map<int, int> hx;
+        stack<int> stk;
+        vector<int> p;
+        for(int i = nums2.size()-1; i >= 0; i--){
+            while(!stk.empty() && nums2[i] > stk.top()){
+                stk.pop();
+            }
+            hx[nums2[i]] = stk.empty() ? -1 : stk.top();
+            stk.push(nums2[i]);
+        }
+
+        for(int i = 0; i < nums1.size(); i++){
+            p.push_back(hx[nums1[i]]);
+        }
+        return p;
+    }
+};
