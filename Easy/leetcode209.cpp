880b7c01f06469ac2091650a26e05855d43a6f04
Finger Frings <67583493+FIngerFrings@users.noreply.github.com>
2021-05-26 20:25:34 +0800

    Update leetcode209.cpp

--- Easy/leetcode209.cpp
+++ Easy/leetcode209.cpp
@@ -37,3 +37,51 @@ public:
         return length;
     }
 };
+
+//方法二 暴力解法，使用两个for循环
+//需要注意的一个地方是，可以使用INT_MAX作为长度初值，这样就不用像方法一一样还要判断以下length是否为0
+class Solution {
+public:
+    int minSubArrayLen(int s, vector<int>& nums) {
+        int n = nums.size();
+        if (n == 0) {
+            return 0;
+        }
+        int ans = INT_MAX;
+        for (int i = 0; i < n; i++) {
+            int sum = 0;
+            for (int j = i; j < n; j++) {
+                sum += nums[j];
+                if (sum >= s) {
+                    ans = min(ans, j - i + 1);
+                    break;          //找到从i下标开始符合条件的数组后就不用继续下去了，因为数组都是正数，所以再往下加也都是正数
+                }
+            }
+        }
+        return ans == INT_MAX ? 0 : ans;
+    }
+};
+
+//方法一的改进，同样也是滑动窗口
+//官方解法，原理和方法一一样，但是更好
+class Solution {
+public:
+    int minSubArrayLen(int target, vector<int>& nums) {
+        int n = nums.size();
+        if(n == 0){
+            return 0;
+        }
+        int length = INT_MAX;
+        int l = 0, r = 0, sum = 0;
+        while(r < n){           //没想到这种两个while循环的形式，实际上确实可以每次循环都可以增加r，如果满足条件，可以再减少l
+            sum += nums[r];
+            while(sum >= target){
+                length = (length < (r - l + 1) ? length : (r - l + 1));
+                sum -= nums[l];
+                l++;
+            }
+            r++;
+        }
+        return (length == INT_MAX) ? 0 : length;
+    }
+};
