b14ff4f553d77da7e8bade23ef64bd61c4c28257
Finger Frings <67583493+FIngerFrings@users.noreply.github.com>
2021-05-26 22:37:07 +0800

    Update leetcode509.cpp

--- Easy/leetcode509.cpp
+++ Easy/leetcode509.cpp
@@ -13,3 +13,39 @@ public:
         }
     }
 };
+
+
+//方法二，动态规划
+class Solution {
+public:
+    int fib(int n) {
+        if(n < 2){
+            return n;
+        }
+        int *nums = new int[n + 1];
+        nums[0] = 0;
+        nums[1] = 1;
+        for(int i = 2; i <= n; i++){
+            nums[i] = nums[i-1] + nums[i-2];
+        }
+        return nums[n];
+    }
+};
+
+class Solution {
+public:
+    int fib(int n) {
+        if(n < 2){
+            return n;
+        }
+        int p = 0, q = 0, r = 1;
+        for(int i = 2; i <= n; i++){
+            p = q;
+            q = r;
+            r = p + q;
+        }
+        return r;
+    }
+};
+
+//两种动态规划其实原理是相同的
