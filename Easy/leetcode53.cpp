f35b17f007df78754f85cacef5dc0cc4e66ee766
Finger Frings <67583493+FIngerFrings@users.noreply.github.com>
2021-05-27 21:16:07 +0800

    Update leetcode53.cpp

--- Easy/leetcode53.cpp
+++ Easy/leetcode53.cpp
@@ -41,3 +41,34 @@ public:
         return maxans;
     }
 };
+
+//方法三，分治
+//具体的还是去看官方解释，太多了
+class Solution {
+public:
+    struct Status {
+        int lSum, rSum, mSum, iSum;
+    };
+
+    Status pushUp(Status l, Status r) {
+        int iSum = l.iSum + r.iSum;
+        int lSum = max(l.lSum, l.iSum + r.lSum);
+        int rSum = max(r.rSum, r.iSum + l.rSum);
+        int mSum = max(max(l.mSum, r.mSum), l.rSum + r.lSum);
+        return (Status) {lSum, rSum, mSum, iSum};
+    };
+
+    Status get(vector<int> &a, int l, int r) {
+        if (l == r) {
+            return (Status) {a[l], a[l], a[l], a[l]};
+        }
+        int m = (l + r) >> 1;
+        Status lSub = get(a, l, m);
+        Status rSub = get(a, m + 1, r);
+        return pushUp(lSub, rSub);
+    }
+
+    int maxSubArray(vector<int>& nums) {
+        return get(nums, 0, nums.size() - 1).mSum;
+    }
+};
