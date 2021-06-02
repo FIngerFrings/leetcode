ae6cae31cdc9bc981880c82c71841e11879fbb4d
Finger Frings <67583493+FIngerFrings@users.noreply.github.com>
2021-06-02 11:04:42 +0800

    Update leetcode77.cpp

--- Easy/leetcode77.cpp
+++ Easy/leetcode77.cpp
@@ -9,6 +9,7 @@ public:
     vector<vector<int>> ans;
 
     void backtacking(int curr, int n, int k){
+        //这个判断条件是没想到的，可以减少计算次数
         if(t.size() + (n - curr + 1) < k){
             return;
         }
@@ -32,6 +33,44 @@ public:
 
     vector<vector<int>> combine(int n, int k) {
         backtacking(1, n, k);
+        return ans;
+    }
+};
+
+//方法二：非递归（字典序法）实现组合型枚举，
+//思路：实际上就和leetcode78中的mask解法一样，只不过这里因为限定了k个数，所以mask并不能像leetcode78中一样直接+1，而是有一定规律的
+//规律如下：表示数组的二进制数的最低位为1时，如果末尾有t个连续的1，则直接将倒数第t位的1和倒数第t+1位的0替换
+//如果最低位为0，假设末尾有t个连续的0，这t个连续的0前面有m个连续的1，则将倒数第t+m位的1和倒数t+m+1位的0对换，然后将后面连续的1全部移动到最低位
+//以上就是mask的增加方式。在实际实现中，我们实际上是从低到高的找第一个j，使得t[j]+1 != t[j+1]，这说明t[j]和t[j+1]之间是有0的，因此我们就可以将t[j]+1，然后后面连续的1移动到最后
+//在本题中，我们使用t来临时保存子集，其中最重要的一环就是如何判断全部枚举完了，我们使用的是将t的第k位保存为n+1，当通过寻找发现第一个j=k时，就说明到最后了
+class Solution {
+public:
+    vector<int> t;
+    vector<vector<int>> ans;
+    
+    vector<vector<int>> combine(int n, int k) {
+        //1. 首先将前k位放进t中
+        for(int i = 1; i <= k; i++){
+            t.emplace_back(i);
+        }
+        
+        //2. 将n+1保存在t的第k位，方便判断循环的终止条件
+        t.emplace_back(n+1);
+        int j = 0;
+        while(j < k){
+            //3. 将t的前k位插入ans
+            ans.emplace_back(t.begin(), t.begin()+k);
+            
+            //记得将j复位
+            j = 0;
+            //将mask移动到下一位
+            while(j < k && t[j]+1 == t[j+1]){
+                t[j] = j+1;
+                j++;
+            }
+
+            ++t[j];
+        }
         return ans;
     }
 };
