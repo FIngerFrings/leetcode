a5f20ebafe3af04211537a5e8420e6aa45811f56
Finger Frings <67583493+FIngerFrings@users.noreply.github.com>
2021-06-05 16:24:51 +0800

    Update leetcode210.cpp

--- Easy/leetcode210.cpp
+++ Easy/leetcode210.cpp
@@ -57,3 +57,50 @@ public:
         ans.push_back(i);
     }
 };
+
+//方法二：拓扑排序+BFS
+//思路：方法一的深度优先搜索是一种「逆向思维」：最先被放入栈中的节点是在拓扑排序中最后面的节点。我们也可以使用正向思维，顺序地生成拓扑排序，这种方法也更加直观。
+//我们考虑拓扑排序中最前面的节点，该节点一定不会有任何入边，也就是它没有任何的先修课程要求。
+//当我们将一个节点加入答案中后，我们就可以移除它的所有出边，代表着它的相邻节点少了一门先修课程的要求。
+//如果某个相邻节点变成了「没有任何入边的节点」，那么就代表着这门课可以开始学习了。
+//按照这样的流程，我们不断地将没有入边的节点加入答案，直到答案中包含所有的节点（得到了一种拓扑排序）或者不存在没有入边的节点（图中包含环）。
+//实际上也是和之前的BFS一样，使用while循环+队列，关键就是要记录每个节点的入度，当入度为0时，说明可以学习这门课了，因此也就可以加入队列
+//同时将其他与它相连的节点的入度-1，如果有满足入度为0的节点，同样加入队列中
+class Solution {
+public:
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> edges(numCourses);
+        vector<int> indeg(numCourses);
+        vector<int> ans;
+        for(auto &info : prerequisites){
+            edges[info[1]].push_back(info[0]);
+            indeg[info[0]]++;
+        }
+
+        queue<int> q;
+        for(int i = 0; i < numCourses; i++){
+            if(indeg[i] == 0){
+                q.push(i);
+            }
+        }
+
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            ans.push_back(node);
+            for(int j : edges[node]){
+                indeg[j]--;
+                if(indeg[j] == 0){
+                    q.push(j);
+                }
+            }
+        }
+
+        if(ans.size() != numCourses){
+            return {};
+        }
+        return ans;
+
+
+    }
+};
