4b80d2e03f0f3fae95e9b3019ca0a4807314f6fb
Finger Frings <67583493+FIngerFrings@users.noreply.github.com>
2021-06-05 23:42:33 +0800

    Update leetcode221.cpp

--- Easy/leetcode221.cpp
+++ Easy/leetcode221.cpp
@@ -42,3 +42,32 @@ public:
         return maxside * maxside;
     }
 };
+
+//方法二：动态规划
+class Solution {
+public:
+    int maximalSquare(vector<vector<char>>& matrix) {
+        int maxside = 0;
+        if(matrix.size() == 0 || matrix[0].size() == 0){
+            return 0;
+        }
+        int row = matrix.size();
+        int col = matrix[0].size();
+        vector<vector<int>> dp(row, vector<int>(col));
+
+        for(int i = 0; i < row; i++){
+            for(int j = 0; j < col; j++){
+                if(matrix[i][j] == '1'){
+                    if(i == 0 || j == 0){
+                        dp[i][j] = 1;
+                    }
+                    else{
+                        dp[i][j] = min(min(dp[i-1][j], dp[i][j-1]), dp[i-1][j-1]) + 1;
+                    }
+                    maxside = max(maxside, dp[i][j]);
+                }
+            }
+        }
+        return maxside * maxside;
+    }
+};
