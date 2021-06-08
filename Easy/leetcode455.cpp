fea2a1af9ee918bf560883897219265b55e1028c
Finger Frings <67583493+FIngerFrings@users.noreply.github.com>
2021-06-08 15:40:33 +0800

    Create leetcode455.cpp

---
+++ Easy/leetcode455.cpp
@@ -0,0 +1,42 @@
+/* 
+ * 假设你是一位很棒的家长，想要给你的孩子们一些小饼干。但是，每个孩子最多只能给一块饼干。
+ * 对每个孩子 i，都有一个胃口值 g[i]，这是能让孩子们满足胃口的饼干的最小尺寸；并且每块饼干 j，都有一个尺寸 s[j] 。
+ * 如果 s[j] >= g[i]，我们可以将这个饼干 j 分配给孩子 i ，这个孩子会得到满足。你的目标是尽可能满足越多数量的孩子，并输出这个最大数值。
+ */
+//方法一：排序+贪心
+//思路：因为饥饿度最小的孩子最容易吃饱，所以我们先考虑这个孩子。为了尽量使得剩下的饼干可以满足饥饿度更大的孩子，所以我们应该把大于等于这个孩子饥饿度的、且大小最小的饼干给这
+//个孩子。满足了这个孩子之后，我们采取同样的策略，考虑剩下孩子里饥饿度最小的孩子，直到没有满足条件的饼干存在。
+//简而言之，这里的贪心策略是，给剩余孩子里最小饥饿度的孩子分配最小的能饱腹的饼干。
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        sort(g.begin(), g.end());
+        sort(s.begin(), s.end());
+        int i = 0, j = 0, num = 0;
+        while(i < g.size() && j < s.size()){
+            if(g[i] <= s[j]){
+                ++i;
+                ++num;
+            }
+            ++j;
+        }
+        return num;
+    }
+};
+
+//不需要num变量保存小孩个数，可以直接返回i
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        sort(g.begin(), g.end());
+        sort(s.begin(), s.end());
+        int i = 0, j = 0;
+        while(i < g.size() && j < s.size()){
+            if(g[i] <= s[j]){
+                ++i;
+            }
+            ++j;
+        }
+        return i;
+    }
+};
