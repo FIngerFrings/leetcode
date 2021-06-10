8c678ed489ead511dee32a2ea84319c58bd8de9f
Finger Frings <67583493+FIngerFrings@users.noreply.github.com>
2021-06-10 11:21:01 +0800

    Update leetcode142.cpp

--- Easy/leetcode142.cpp
+++ Easy/leetcode142.cpp
@@ -31,3 +31,88 @@ public:
         return NULL;
     }
 };
+//这是官方给出的哈希表解法，实际上和上面的一样
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        unordered_set<ListNode *> visited;
+        while (head != nullptr) {
+            if (visited.count(head)) {
+                return head;
+            }
+            visited.insert(head);
+            head = head->next;
+        }
+        return nullptr;
+    }
+};
+
+//方法二：双指针
+//思路：使用快慢指针判断是否有环，如果有环，则将fast重置为head，然后slow和fast都一步一步跳，相遇的地方就是入环的节点
+//证明见官方解答或markdown笔记
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        if(head == nullptr || head->next == nullptr)    return NULL;
+        ListNode *slow = head;
+        ListNode *fast = head;
+        while(fast->next != nullptr && fast->next->next != nullptr){
+            slow = slow->next;
+            fast = fast->next->next;
+            if(slow == fast){
+                fast = head;
+                break;
+            }
+        }
+        if(fast->next == nullptr || fast->next->next == nullptr){
+            return NULL;
+        }
+        while(fast != slow){
+            fast = fast->next;
+            slow = slow->next;
+        }
+        return fast;
+        
+    }
+};
+
+//官方比较间接的双指针
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        ListNode *slow = head;
+        ListNode *fast = head;
+        while(fast != nullptr){
+            slow = slow->next;
+            if(fast->next == nullptr){
+                return nullptr;
+            }
+            fast = fast->next->next;
+            if(fast == slow){
+                fast = head;
+                while(fast != slow){
+                    fast = fast->next;
+                    slow = slow->next;
+                }
+                return fast;
+            }
+        }
+        return nullptr;
+    }
+};
