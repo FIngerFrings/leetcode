45f00cf1c8fbf2b470f51315db028b9152b34e87
Finger Frings <67583493+FIngerFrings@users.noreply.github.com>
2021-06-13 23:23:16 +0800

    Create leetcode69.cpp

---
+++ Easy/leetcode69.cpp
@@ -0,0 +1,42 @@
+/* 
+ * 实现 int sqrt(int x) 函数。
+ * 计算并返回 x 的平方根，其中 x 是非负整数。
+ * 由于返回类型是整数，结果只保留整数的部分，小数部分将被舍去。
+ */
+
+//方法一：自己写的while循环
+//思路：通过while循环得到i
+class Solution {
+public:
+    int mySqrt(int x) {
+        long i = 1;
+        while(i * i <= x){
+            ++i;
+        }
+        return i * i == x ? i : i - 1;
+    }
+};
+
+//方法二：二分查找
+//思路：在1到x内二分查找
+class Solution {
+public:
+    int mySqrt(int x) {
+        if(x == 0)  return 0;
+        long l = 1, r = x;
+        while(l <= r){
+            long mid = (l + r) / 2;
+            long long num = mid * mid;
+            if(num == x){
+                return mid;
+            }
+            else if(num < x){
+                l = mid + 1;
+            }
+            else{
+                r = mid - 1;
+            }
+        }
+        return l * l < x ? l : l - 1;
+    }
+};
