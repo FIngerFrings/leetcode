2d4e1ca713b2d1cce4e2d5c8b990a817877f6f17
Finger Frings <67583493+FIngerFrings@users.noreply.github.com>
2021-06-13 21:27:41 +0800

    Update leetcode524.cpp

--- Easy/leetcode524.cpp
+++ Easy/leetcode524.cpp
@@ -40,7 +40,7 @@ public:
         return ans.empty() ? "" : ans;
     }
 };
-//方法一：暴力枚举（超出时间限制）
+//方法一：递归枚举（超出时间限制）
 //思路：列举出s所能组成的所有字符串，之后遍历dictionary，并选出最长字典序最小的字符串
 class Solution {
 public:
@@ -57,6 +57,39 @@ public:
     string findLongestWord(string s, vector<string>& dictionary) {
         generate(s, 0, "");
         int maxsize = 0;    //为什么这里不能用INT_MIN
+        string ans = "";
+        for(string str : dictionary){
+            if(q[str]){
+                if(str.size() > maxsize){
+                    ans = str;
+                    maxsize = str.size();
+                }
+                else if(str.size() == maxsize && str < ans){
+                    ans = str;
+                }
+            }
+        }
+        return ans;
+    }
+};
+
+//方法二：迭代枚举
+//思路：和上面相同，也是列出所有s可能的组合，然后再从中寻找，不过这里使用迭代的方法，使用二进制数得到所有组合
+class Solution {
+public:
+    string findLongestWord(string s, vector<string>& dictionary) {
+        unordered_map<string, bool> q;
+        int n = s.size();
+        for(int i = 1; i <= 2^n; i++){
+            string k = "";
+            for(int j = 0; j < n; j++){
+                if(i & s[j]){
+                    k += s[j];
+                }
+            }
+            q[k] = true;
+        }
+        int maxsize = 0;
         string ans = "";
         for(string str : dictionary){
             if(q[str]){
