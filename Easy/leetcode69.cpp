f29c2f8381f76fa6423c85054aa3ee236ac4c580
Finger Frings <67583493+FIngerFrings@users.noreply.github.com>
2021-06-14 21:06:33 +0800

    Update leetcode69.cpp

--- Easy/leetcode69.cpp
+++ Easy/leetcode69.cpp
@@ -58,3 +58,20 @@ public:
         return ans;
     }
 };
+
+//方法二：牛顿迭代法
+class Solution {
+public:
+    int mySqrt(int x) {
+        if(x == 0)  return 0;
+        double C = x, x0 = x;
+        while(true){
+            double xi = 0.5 * (x0 + C / x0);
+            if(fabs(xi - x0) <= 1e-7){
+                break;
+            }
+            x0 = xi;
+        }
+        return (int)x0;
+    }
+};
