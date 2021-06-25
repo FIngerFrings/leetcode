d0629a08bd8c68b5b3910df8b34b052b7cb28cc4
Finger Frings <67583493+FIngerFrings@users.noreply.github.com>
2021-06-25 10:47:56 +0800

    Update leetcode198.cpp

--- Easy/leetcode198.cpp
+++ Easy/leetcode198.cpp
@@ -28,3 +28,34 @@ public:
         return dp[n];
     }
 };
+//比较规范的写法
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        if(nums.empty())  return 0;
+        int n = nums.size();
+        vector<int> dp(n + 1);
+        dp[1] = nums[0];
+        for(int i = 2; i <= n; i++){
+            dp[i] = max(dp[i-1], dp[i-2] + nums[i-1]);
+        }
+        return dp[n];
+    }
+};
+
+//压缩空间的dp
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        if(nums.empty())  return 0;
+        int n = nums.size();
+        if(n == 1)  return nums[0];
+        int pre2 = 0, pre1 = 0, curr;
+        for(int i = 0; i < n; i++){
+            curr = max(pre2 + nums[i], pre1);
+            pre2 = pre1;
+            pre1 = curr;
+        }
+        return pre1;
+    }
+};
