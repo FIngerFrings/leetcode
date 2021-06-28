0ab4743441e4195faf2da9724a83953b1cd88b1e
Finger Frings <67583493+FIngerFrings@users.noreply.github.com>
2021-06-28 23:14:14 +0800

    Update leetcode416.cpp

--- Easy/leetcode416.cpp
+++ Easy/leetcode416.cpp
@@ -88,3 +88,24 @@ public:
         return dp[n][target];
     }
 };
+
+//压缩空间后的动态规划
+class Solution {
+public:
+    bool canPartition(vector<int>& nums) {
+        int sum = accumulate(nums.begin(), nums.end(), 0);
+        int n = nums.size();
+        if (sum % 2 || n < 2) return false;
+        int target = sum / 2;
+        vector<bool> dp(target + 1, false);
+        dp[0] = true;
+        for (int i = 1; i <= n; ++i) {
+            for (int j = target; j >= 1; --j) {
+                if(j >= nums[i-1]){
+                    dp[j] = dp[j] || dp[j-nums[i-1]];
+                }
+            }
+        }
+        return dp[target];
+    }
+};
