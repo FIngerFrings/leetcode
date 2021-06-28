2376992abb322648ee088ecb42ff03c1e002c9f8
Finger Frings <67583493+FIngerFrings@users.noreply.github.com>
2021-06-28 23:11:43 +0800

    Update leetcode416.cpp

--- Easy/leetcode416.cpp
+++ Easy/leetcode416.cpp
@@ -57,3 +57,34 @@ public:
         return find;
     }
 };
+
+//方法二：动态规划
+//思路：本题等价于 0-1 背包问题，设所有数字和为 sum，我们的目标是选取一部分物品，使得它们的总和为 sum/2。
+//如果sum不能被2整除，说明不会有子集的和为sum的一半，直接返回false
+//同时，如果数组个数小于2，则说明不能分为两个子集，返回false
+//对于dp[i][j]，如果nums[i-1]大于j，则不能选择nums[i-1]， dp[i][j] = dp[i-1][j]
+//否则就是dp[i][j] = dp[i-1][j] || dp[i-1][j-nums[i-1]];
+class Solution {
+public:
+    bool canPartition(vector<int>& nums) {
+        int sum = accumulate(nums.begin(), nums.end(), 0);
+        int n = nums.size();
+        if (sum % 2 || n < 2) return false;
+        int target = sum / 2, n = nums.size();
+        vector<vector<bool>> dp(n + 1, vector<bool>(target + 1, false));
+        for (int i = 0; i <= n; ++i) {
+            dp[i][0] = true;
+        }
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 1; j <= target; ++j) {
+                if(j >= nums[i-1]){
+                    dp[i][j] = dp[i-1][j] || dp[i-1][j-nums[i-1]];
+                }
+                else{
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+        return dp[n][target];
+    }
+};
