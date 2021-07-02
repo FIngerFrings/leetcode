f3dcd907be215a047c0c0bf8ee23a1da4168549f
Finger Frings <67583493+FIngerFrings@users.noreply.github.com>
2021-07-02 10:09:26 +0800

    Update leetcode714.cpp

--- Easy/leetcode714.cpp
+++ Easy/leetcode714.cpp
@@ -41,3 +41,24 @@ public:
         return n_have;
     }
 };
+
+//方法二：贪心算法
+//思路：这个思路并不是特别懂，详细可以见官方的解法
+class Solution {
+public:
+    int maxProfit(vector<int>& prices, int fee) {
+        int n = prices.size();
+        int buy = fee + prices[0];
+        int profit = 0;
+        for(int i = 1; i < n; i++){
+            if(prices[i] + fee < buy){
+                buy = prices[i] + fee;
+            }
+            else if(prices[i] > buy){
+                profit += prices[i] - buy;
+                buy = prices[i];
+            }
+        }
+        return profit;
+    }
+};
