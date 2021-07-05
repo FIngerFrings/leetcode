6d3f485dc32ffcd563d74ac110c10b71f8e94bb0
Finger Frings <67583493+FIngerFrings@users.noreply.github.com>
2021-07-05 19:25:12 +0800

    Update leetcode470.cpp

--- Easy/leetcode470.cpp
+++ Easy/leetcode470.cpp
@@ -42,3 +42,38 @@ public:
         return idx;
     }
 };
+
+//下面是优化后的算法，利用了被拒绝的数
+// The rand7() API is already defined for you.
+// int rand7();
+// @return a random integer in the range 1 to 7
+
+class Solution {
+public:
+    int rand10() {
+        int row;
+        int col;
+        int idx;
+        while(true){
+            row = rand7();
+            col = rand7();
+            idx = row + (col - 1) * 7;
+            if(idx <= 40){
+                return (idx - 1) % 10 + 1;
+            }
+            row = idx - 40;
+            col = rand7();
+            idx = row + (col - 1) * 7;
+            if(idx <= 60){
+                return (idx - 1) % 10 + 1;
+            }
+            row = idx - 60;
+            col = rand7();
+            idx = row + (col - 1) * 7;
+            if(idx <= 20){
+                return (idx - 1) % 10 + 1;
+            }
+        }
+        return -1;
+    }
+};
