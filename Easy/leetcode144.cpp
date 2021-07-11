86fd21fb7082602acc2cb08e10a411838a279d15
Finger Frings <67583493+FIngerFrings@users.noreply.github.com>
2021-07-11 22:55:46 +0800

    Create leetcode144.cpp

---
+++ Easy/leetcode144.cpp
@@ -0,0 +1,143 @@
+/* 给你二叉树的根节点 root ，返回它节点值的 前序 遍历。 */
+//方法一：递归
+//思路：略
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> ans;
+
+    void qxbl(TreeNode* root){
+        if(!root){
+            return;
+        }
+
+        ans.push_back(root->val);
+        qxbl(root->left);
+        qxbl(root->right);
+    }
+
+    vector<int> preorderTraversal(TreeNode* root) {
+        qxbl(root);
+        return ans;
+    }
+};
+
+//方法二：迭代
+//思路：略，和前面中序遍历的迭代思路差不多，但是我觉得不够好
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        stack<TreeNode *> stk;
+        while(root != nullptr || !stk.empty()){
+            while(root != nullptr){
+                ans.push_back(root->val);
+                if(root->right != nullptr){
+                    stk.push(root->right);
+                }
+                root = root->left;
+            }
+            if(!stk.empty()){
+                root = stk.top();
+                stk.pop();
+            }
+        }
+        return ans;
+    }
+};
+
+//是我写的不好，下面是官方的迭代，思路差不多
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        stack<TreeNode *> stk;
+        while(root != nullptr || !stk.empty()){
+            while(root != nullptr){
+                ans.push_back(root->val);
+                stk.push(root);
+                root = root->left;
+            }
+            root = stk.top();
+            stk.pop();
+            root = root->right;
+        }
+        return ans;
+    }
+};
+
+//方法三：moris前序遍历
+//思路：略
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        TreeNode *pre = nullptr;
+        while(root != nullptr){
+            if(root->left != nullptr){
+                pre = root->left;
+                while(pre->right != nullptr && pre->right != root){
+                    pre = pre->right;
+                }
+
+                if(pre->right == nullptr){
+                    ans.push_back(root->val);
+                    pre->right = root;
+                    root = root->left;
+                }
+                else{
+                    root = root->right;
+                    pre->right = nullptr;
+                }
+            }
+            else{
+                ans.push_back(root->val);
+                root = root->right;
+            }
+        }
+        return ans;
+    }
+};
