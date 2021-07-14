3708142d1be2ef42f95e69ed84e605c5299a3177
Finger Frings <67583493+FIngerFrings@users.noreply.github.com>
2021-07-14 22:15:20 +0800

    Update leetcode84.cpp

--- Easy/leetcode84.cpp
+++ Easy/leetcode84.cpp
@@ -54,7 +54,7 @@ public:
         stack<int> stk;
 
         for(int i = 0; i < n; i++){
-            if(!stk.empty() && heights[stk.top()] > heights[i]){
+            while(!stk.empty() && heights[stk.top()] >= heights[i]){
                 stk.pop();
             }
             left[i] = (stk.empty()) ? -1 : stk.top();
@@ -63,10 +63,38 @@ public:
 
         stk = stack<int>();
         for(int i = n-1; i >= 0; i--){
-            if(!stk.empty() && heights[stk.top()] > heights[i]){
+            while(!stk.empty() && heights[stk.top()] >= heights[i]){
                 stk.pop();
             }
             right[i] = (stk.empty()) ? n : stk.top();
+            stk.push(i);
+        }
+
+        int ans = 0;
+        for(int i = 0; i < n; i++){
+            ans = max(ans, (right[i] - left[i] - 1) * heights[i]);
+        }
+        return ans;
+
+
+    }
+};
+
+//单调栈+节省时间
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& heights) {
+        int n = heights.size();
+        vector<int> left(n), right(n, n);
+
+        stack<int> stk;
+
+        for(int i = 0; i < n; i++){
+            while(!stk.empty() && heights[stk.top()] >= heights[i]){
+                right[stk.top()] = i;
+                stk.pop();
+            }
+            left[i] = (stk.empty()) ? -1 : stk.top();
             stk.push(i);
         }
 
