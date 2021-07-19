a0d44bbc10d0c4f94d1fde826f73c17087cc6f5a
Finger Frings <67583493+FIngerFrings@users.noreply.github.com>
2021-07-19 16:05:25 +0800

    Create leetcode160.cpp

---
+++ Easy/leetcode160.cpp
@@ -0,0 +1,96 @@
+/*
+ * 给你两个单链表的头节点 headA 和 headB ，请你找出并返回两个单链表相交的起始节点。如果两个链表没有交点，返回 null 。
+ */
+//方法一：哈希表
+//思路：略
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        if(headA == nullptr || headB == nullptr)    return nullptr;
+        unordered_set<ListNode *> hash;
+        while(headA != nullptr || headB != nullptr){
+            if(headA != nullptr){
+                if(hash.count(headA)){
+                    return headA;
+                }
+                else{
+                    hash.insert(headA);
+                    headA = headA->next;
+                }
+            }
+
+            if(headB != nullptr){
+                if(hash.count(headB)){
+                    return headB;
+                }
+                else{
+                    hash.insert(headB);
+                    headB = headB->next;
+                }
+                
+            }
+        }
+        return nullptr;
+    }
+};
+
+//官方的哈希表
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        unordered_set<ListNode *> hash;
+        while(headA != nullptr){
+            hash.insert(headA);
+            headA = headA->next;
+        }
+
+        while(headB != nullptr){
+            if(hash.count(headB)){
+                return headB;
+            }
+            headB = headB->next;
+        }
+        return nullptr;
+    }
+};
+
+//方法二：双指针
+//思路：使用两个指针分别指向headA和headB，假设两个链表有相交点，相交点之前的链表长度分别为a和b，相交之后的长度为c
+//则两个指针分别前进，当l1为空时就指向headB，l2为空时指向headA，则到相交点两个指针经过的节点数都为a+b+c
+//如果没有相交节点，则两个指针会同时到达空节点
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        ListNode *l1 = headA;
+        ListNode *l2 = headB;
+        while(l1 != l2){
+            l1 = l1 ? l1->next : headB;
+            l2 = l2 ? l2->next : headA;
+        }
+        return l1;
+    }
+};
