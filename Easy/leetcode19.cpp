c6c0a116fe677fb16157c4219af65801c635b3e0
Finger Frings <67583493+FIngerFrings@users.noreply.github.com>
2021-07-25 21:41:17 +0800

    Update leetcode19.cpp

--- Easy/leetcode19.cpp
+++ Easy/leetcode19.cpp
@@ -37,3 +37,75 @@ public:
         return head;
     }
 };
+
+//方法二：栈
+//思路：使用栈将所有节点放入，然后依次弹出栈，弹出n个节点后，栈顶节点就是要删除节点的前面一个节点
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode *dummyNode = new ListNode();
+        dummyNode->next = head;
+        stack<ListNode *> stk;
+        ListNode *ptr = dummyNode;
+        while(ptr){
+            stk.push(ptr);
+            ptr = ptr->next;
+        }
+
+        int i = 0;
+        while(i < n){
+            ++i;
+            stk.pop();
+        }
+        ptr = stk.top();
+        ptr->next = ptr->next->next;
+        return dummyNode->next;
+    }
+};
+
+//方法三：快慢指针
+//思路：可以设定两个指针，一个指针指向哑节点，一个指针指向头节点
+//快指针先移动n步，然后两个指针一起移动，当快指针到达链表尾时
+//满指针到达倒数n个节点的前面那个节点
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode *dummynode = new ListNode();
+        dummynode->next = head;
+        ListNode *fast = head;
+        ListNode *low = dummynode;
+        int i = 0;
+        while(i < n){
+            ++i;
+            fast = fast->next;
+        }
+
+        while(fast){
+            fast = fast->next;
+            low = low->next;
+        }
+
+        low->next = low->next->next;
+        return dummynode->next;
+    }
+};
