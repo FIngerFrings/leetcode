f3b96b1b0ac5f91560f7f31f46ce0ee9da37b816
Finger Frings <67583493+FIngerFrings@users.noreply.github.com>
2021-08-05 11:59:27 +0800

    Create leetcode617.cpp

---
+++ Easy/leetcode617.cpp
@@ -0,0 +1,34 @@
+/*
+ * 给定两个二叉树，想象当你将它们中的一个覆盖到另一个上时，两个二叉树的一些节点便会重叠。
+ * 你需要将他们合并为一个新的二叉树。合并的规则是如果两个节点重叠，那么将他们的值相加作为节点合并后的新值，否则不为 NULL 的节点将直接作为新二叉树的节点。
+ */
+//方法一：递归
+//思路：将两个结点相加，并递归计算结点的左右子树
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* mergeTrees(TreeNode* root1, TreeNode* root2) {
+        if(root1 && root2){
+            root1->val += root2->val;
+        }
+        else if(root1 || root2){
+            return root1 ? root1 : root2;
+        }
+        else{
+            return nullptr;
+        }
+        root1->left = mergeTrees(root1->left, root2->left);
+        root1->right = mergeTrees(root1->right, root2->right);
+        return root1;
+    }
+};
