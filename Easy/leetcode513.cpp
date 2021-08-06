719b06020e256c24c3930aee4c2c7a29d001ad2a
Finger Frings <67583493+FIngerFrings@users.noreply.github.com>
2021-08-06 17:33:20 +0800

    Update leetcode513.cpp

--- Easy/leetcode513.cpp
+++ Easy/leetcode513.cpp
@@ -33,3 +33,45 @@ public:
         return node->val;
     }
 };
+
+//实际上题目要求的是最底层最左边的结点，并不是说明它就是左结点，所以这里也有可能是右节点
+//因此只需要递归到最底层，同时先处理左结点即可
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int ans;
+    int maxlen = -1;
+
+    void findBottomHelper(TreeNode *root, int level){
+        if(!root->left && !root->right){
+            if(level > maxlen){
+                ans = root->val;
+                maxlen = level;
+            }
+            return;
+        }
+
+        if(root->left){
+            findBottomHelper(root->left, level + 1);
+        }
+
+        if(root->right){
+            findBottomHelper(root->right, level + 1);
+        }
+    }
+
+    int findBottomLeftValue(TreeNode* root) {
+        findBottomHelper(root, 0);
+        return ans;
+    }
+};
