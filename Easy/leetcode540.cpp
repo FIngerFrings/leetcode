764e779c61775941f9a949aa92a7836d4cedf0af
Finger Frings <67583493+FIngerFrings@users.noreply.github.com>
2021-09-12 11:02:12 +0800

    Update leetcode540.cpp

--- Easy/leetcode540.cpp
+++ Easy/leetcode540.cpp
@@ -30,3 +30,37 @@ public:
         return nums.back();
     }
 };
+
+//方法三：二分查找
+//思路：详细看官方解答
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        int n = nums.size();
+        int l = 0, r = n - 1;
+        while(l < r){
+            int mid = (l + r) / 2;
+            bool halareeven = (r - mid) % 2 == 0;
+            if(nums[mid] == nums[mid + 1]){
+                if(halareeven){
+                    l = mid + 2;
+                }
+                else{
+                    r = mid - 1;
+                }
+            }
+            else if(nums[mid] == nums[mid - 1]){
+                if(halareeven){
+                    r = mid - 2;
+                }
+                else{
+                    l = mid + 1;
+                }
+            }
+            else{
+                return nums[mid];
+            }
+        }
+        return nums[l];
+    }
+};
