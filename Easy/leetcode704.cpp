0ac6a2570760c5fecff482ac229a44c928fab312
Finger Frings <67583493+FIngerFrings@users.noreply.github.com>
2021-11-18 15:49:28 +0800

    Update leetcode704.cpp

--- Easy/leetcode704.cpp
+++ Easy/leetcode704.cpp
@@ -21,4 +21,44 @@ public:
 };
 
 //上面的写法不太规范，规范方法如下
+//代码随想录左闭右闭方式
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int l = 0, r = nums.size() - 1;
+        while(l <= r){
+            int mid = l + (r - l) / 2;
+            if(nums[mid] > target){
+                r = mid - 1;
+            }
+            else if(nums[mid] < target){
+                l = mid + 1;
+            }
+            else{
+                return mid;
+            }
+        }
+        return -1;
+    }
+};
 
+//代码随想录左闭右开方式
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int l = 0, r = nums.size();
+        while(l < r){
+            int mid = l + (r - l) / 2;
+            if(nums[mid] > target){
+                r = mid;
+            }
+            else if(nums[mid] < target){
+                l = mid + 1;
+            }
+            else{
+                return mid;
+            }
+        }
+        return -1;
+    }
+};
