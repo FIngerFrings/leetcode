ccea2be7fc95834e2e89d90b39a1804357c1c204
Finger Frings <67583493+FIngerFrings@users.noreply.github.com>
2021-11-19 21:32:57 +0800

    Update leetcode844.cpp

--- Easy/leetcode844.cpp
+++ Easy/leetcode844.cpp
@@ -24,3 +24,59 @@ public:
         return ans;
     }
 };
+
+//方法二：双指针
+//思路：因为退格键只会删掉它前面的一个非退格键字符，所以我们从后面开始使用双指针遍历两个字符串
+//遇到退格键则skip+1，当遇到字符时，判断skip是否为0，不是0则代表这个字符是要被删除的
+//所以首先移动到不需要删除的字符位置，然后对比此时两个字符串所不需要删除的字符是否相等，如果不等则直接返回false
+//否则继续遍历下去
+class Solution {
+public:
+    bool backspaceCompare(string s, string t) {
+        int i = s.size() - 1, j = t.size() - 1;
+        int sSkip = 0, tSkip = 0;
+        while(i >= 0 || j >= 0){
+            while(i >= 0){
+                if(s[i] == '#'){
+                    ++sSkip;
+                    --i;
+                }
+                else if (sSkip > 0){
+                    --i;
+                    --sSkip;
+                }
+                else{
+                    break;
+                }
+            }
+
+            while(j >= 0){
+                if (t[j] == '#') {
+                    ++tSkip;
+                    --j;
+                }
+                else if (tSkip > 0){
+                    --tSkip;
+                    --j;
+                }
+                else{
+                    break;
+                }
+            }
+
+            if(i >= 0 && j >= 0){
+                if(s[i] != t[j]){
+                    return false;
+                }
+            }
+            else{
+                if(i >= 0 || j >= 0){
+                    return false;
+                }
+            }
+            --i;
+            --j;
+        }
+        return true;
+    }
+};
