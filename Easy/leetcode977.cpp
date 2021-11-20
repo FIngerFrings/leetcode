4d2988950d23e8ab55fe59300f3d12503f56ccdc
Finger Frings <67583493+FIngerFrings@users.noreply.github.com>
2021-11-20 16:03:24 +0800

    Update leetcode977.cpp

--- Easy/leetcode977.cpp
+++ Easy/leetcode977.cpp
@@ -14,3 +14,24 @@ public:
         return nums;
     }
 };
+
+//方法二：双指针
+//思路：设定两个指针分别指向数组开头和结尾，然后比较两个数的平方，将较大的数放在要返回的数组的最后
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> ans(n);
+        for(int i = 0, j = n - 1, pos = n - 1; i <= j;){
+            if(nums[i] * nums[i] > nums[j] * nums[j]){
+                ans[pos--] = nums[i] * nums[i];
+                ++i;
+            }
+            else{
+                ans[pos--] = nums[j] * nums[j];
+                --j;
+            }
+        }
+        return ans;
+    }
+};
