3f222e10311b42d9e88edfa00ca5937106bd5e4c
Finger Frings <67583493+FIngerFrings@users.noreply.github.com>
2021-11-22 11:14:41 +0800

    Update leetcode203.cpp

--- Easy/leetcode203.cpp
+++ Easy/leetcode203.cpp
@@ -45,3 +45,112 @@ class Solution {
 //这里的指针打上引号是因为并不一定是传统意义上的指针，例如在对数组进行操作的时候，就可以用一个int类型的数据充当指针
 //对于删除链表中的元素，如果删除的结点在链表中间则非常简单，直接将前面一个结点的next指针指向删除结点的next
 //而如果是链表中的第一个指针，因为他没有前一个结点，所以可以在链表第一个结点前面添加一个哨兵结点，添加哨兵结点的目的是为了让第一个结点和后面节点的处理方式相同
+//方法：迭代，但是代码不够简洁，并且没有释放内存
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+        ListNode *dummyNode = new ListNode();
+        dummyNode->next = head;
+        ListNode *pre = dummyNode;
+        while(head != nullptr){
+            if(head->val == val){
+                pre->next = head->next;
+            }
+            else{
+                pre = head;
+            }
+            head = head->next;
+        }
+        return dummyNode->next;
+    }
+};
+//方法：迭代，官方解法更加简洁，但是仍然没有释放内存
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+        ListNode *dummyNode = new ListNode(0, head);
+        ListNode *pre = dummyNode;
+        while(pre->next != nullptr){
+            if(pre->next->val == val){
+                pre->next = pre->next->next;
+            }
+            else{
+                pre = pre->next;
+            }
+        }
+        return dummyNode->next;
+    }
+};
+//方法：迭代，代码随想录的方法，释放了内存
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+        ListNode *dummyNode = new ListNode(0, head);
+        ListNode *pre = dummyNode;
+        while(pre->next != nullptr){
+            if(pre->next->val == val){
+                ListNode *temp = pre->next;
+                pre->next = pre->next->next;
+                delete temp;
+            }
+            else{
+                pre = pre->next;
+            }
+        }
+        ListNode *newHead = dummyNode->next;
+        delete dummyNode;
+        return newHead;
+    }
+};
+
+//方法：递归
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+        if(head == nullptr){
+            return head;
+        }
+        head->next = removeElements(head->next, val);
+        return head->val == val ? head->next : head;
+    }
+};
