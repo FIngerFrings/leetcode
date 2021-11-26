9cdcdb970f5b3191a7d5e4b4a634d5f7fead16a3
Finger Frings <67583493+FIngerFrings@users.noreply.github.com>
2021-11-26 17:29:05 +0800

    Update leetcode20.cpp

--- Easy/leetcode20.cpp
+++ Easy/leetcode20.cpp
@@ -33,4 +33,18 @@ public:
         return stk.empty();
     }
 };
-
+//代码随想录的栈
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> stk;
+        for(char c : s){
+            if(c == '(')    stk.push(')');
+            else if(c == '[')   stk.push(']');
+            else if(c == '{')   stk.push('}');
+            else if(stk.empty() || stk.top() != c)  return false;
+            else    stk.pop();
+        }
+        return stk.empty();
+    }
+};
