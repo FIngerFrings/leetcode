965dc5510b536283b8b1a10accac19b4d7a8880d
Finger Frings <67583493+FIngerFrings@users.noreply.github.com>
2021-11-28 20:36:35 +0800

    Create leetcode117.cpp

---
+++ Easy/leetcode117.cpp
@@ -0,0 +1,48 @@
+/* 
+ * 给定一个二叉树
+ * 填充它的每个 next 指针，让这个指针指向其下一个右侧节点。如果找不到下一个右侧节点，则将 next 指针设置为 NULL。
+ * 初始状态下，所有 next 指针都被设置为 NULL。
+ */
+//方法一：层序遍历
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+*/
+
+class Solution {
+public:
+    Node* connect(Node* root) {
+        if(root == nullptr) return nullptr;
+        queue<Node *> q;
+        q.push(root);
+        while(!q.empty()){
+            int n = q.size();
+            while(n--){
+                auto node = q.front();
+                q.pop();
+                if(n == 0){
+                    node->next = nullptr;
+                }
+                else{
+                    node->next = q.front();
+                }
+                if(node->left) q.push(node->left);
+                if(node->right) q.push(node->right);
+            }
+        }
+        return root;
+    }
+};
