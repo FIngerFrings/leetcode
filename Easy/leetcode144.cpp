779406f65d1a104cd32e715d697abd4f59348769
Finger Frings <67583493+FIngerFrings@users.noreply.github.com>
2021-11-28 18:32:10 +0800

    Update leetcode144.cpp

--- Easy/leetcode144.cpp
+++ Easy/leetcode144.cpp
@@ -141,3 +141,99 @@ public:
         return ans;
     }
 };
+
+//代码随想录的三种遍历方法
+//递归
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void Traversal(vector<int> &ans, TreeNode *root){
+        if(root == nullptr) return;
+        ans.push_back(root->val);
+        Traversal(ans, root->left);
+        Traversal(ans, root->right);
+    }
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        Traversal(ans, root);
+        return ans;
+    }
+};
+//迭代
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        stack<TreeNode *> stk;
+        vector<int> ans;
+        if(root == nullptr) return {};
+        stk.push(root);
+        while(!stk.empty()){
+            auto node = stk.top();
+            stk.pop();
+            ans.push_back(node->val);
+            if(node->right) stk.push(node->right);
+            if(node->left) stk.push(node->left);
+        }
+        return ans;
+    }
+};
+//统一迭代
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        stack<TreeNode *> stk;
+        if(root == nullptr) return {};
+        vector<int> ans;
+        stk.push(root);
+        while(!stk.empty()){
+            TreeNode *node = stk.top();
+            if(node != nullptr){
+                stk.pop();
+                if(node->right) stk.push(node->right);
+                if(node->left) stk.push(node->left);
+
+                stk.push(node);
+                stk.push(nullptr);
+            }
+            else{
+                stk.pop();
+                node = stk.top();
+                stk.pop();
+                ans.push_back(node->val);
+            }
+        }
+        return ans;
+    }
+};
