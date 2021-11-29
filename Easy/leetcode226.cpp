eb5c25399f4ba4b091ca86275a6bc85e22dcc9cf
Finger Frings <67583493+FIngerFrings@users.noreply.github.com>
2021-11-29 16:10:41 +0800

    Update leetcode226.cpp

--- Easy/leetcode226.cpp
+++ Easy/leetcode226.cpp
@@ -55,3 +55,64 @@ public:
         return root;
     }
 };
+
+//实际上就是遍历树，然后交换每个节点的左右节点
+//层序遍历
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        queue<TreeNode*> q;
+        if(root == nullptr) return nullptr;
+        q.push(root);
+        while(!q.empty()){
+            int n = q.size();
+            while(n--){
+                auto node = q.front();
+                q.pop();
+                swap(node->right, node->left);
+                if(node->left) q.push(node->left);
+                if(node->right) q.push(node->right);
+            }
+        }
+        return root;
+    }
+};
+//DFS
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        stack<TreeNode*> stk;
+        if(root == nullptr) return nullptr;
+        stk.push(root);
+        while(!stk.empty()){
+            auto node = stk.top();
+            stk.pop();
+            swap(node->left, node->right);
+            if(node->right) stk.push(node->right);
+            if(node->left) stk.push(node->left); 
+        }
+        return root;
+    }
+};
