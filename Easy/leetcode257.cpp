86b204da350a5981ed814c0b7799b99533de828b
Finger Frings <67583493+FIngerFrings@users.noreply.github.com>
2021-11-30 22:01:58 +0800

    Update leetcode257.cpp

--- Easy/leetcode257.cpp
+++ Easy/leetcode257.cpp
@@ -39,7 +39,35 @@ public:
     }
 };
 
-//方法二：BFS
+//递归
+//思路：实际上就是遍历所有节点，当某个节点没有左右字节点时就将path压入ans中
+//实际上是一种回溯
+class Solution {
+public:
+    vector<string> ans;
+    void binaryTreePathsHelper(TreeNode *root, string path){
+        if(root->left == nullptr && root->right == nullptr){
+            ans.push_back(path + to_string(root->val));
+            return;
+        }
+
+        if(root->left){
+            binaryTreePathsHelper(root->left, path + to_string(root->val) + "->");
+        }
+
+        if(root->right){
+            binaryTreePathsHelper(root->right, path + to_string(root->val) + "->");
+        }
+    }
+
+    vector<string> binaryTreePaths(TreeNode* root) {
+        string path = "";
+        binaryTreePathsHelper(root, path);
+        return ans;
+    }
+};
+
+//方法二：队列迭代
 //思路：略，重点是使用两个queue
 /**
  * Definition for a binary tree node.
@@ -81,6 +109,39 @@ public:
                 }
             }
             
+        }
+        return ans;
+    }
+};
+//栈迭代
+class Solution {
+public:
+    vector<string> binaryTreePaths(TreeNode* root) {
+        if(root == nullptr) return {};
+        vector<string> ans;
+        string path = "";
+        stack<TreeNode *> q1;
+        stack<string> q2;
+        q1.push(root);
+        q2.push(to_string(root->val));
+        while(!q1.empty()){
+            auto node = q1.top();
+            q1.pop();
+            auto path = q2.top();
+            q2.pop();
+            if(node->left == nullptr && node->right == nullptr){
+                ans.push_back(path);
+            }
+
+            if(node->left){
+                q1.push(node->left);
+                q2.push(path + "->" + to_string(node->left->val));
+            }
+
+            if(node->right){
+                q1.push(node->right);
+                q2.push(path + "->" + to_string(node->right->val));
+            }
         }
         return ans;
     }
