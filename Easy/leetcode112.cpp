56915598b96dacf5fa51beb5a586822c4d748702
Finger Frings <67583493+FIngerFrings@users.noreply.github.com>
2021-12-02 20:40:52 +0800

    Create leetcode112.cpp

---
+++ Easy/leetcode112.cpp
@@ -0,0 +1,51 @@
+/* 
+ * 给你二叉树的根节点 root 和一个表示目标和的整数 targetSum ，判断该树中是否存在 根节点到叶子节点 的路径，这条路径上所有节点值相加等于目标和 targetSum 。
+ */
+//方法一：递归
+//思路：就是很简单的递归。。。
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasPathSum(TreeNode* root, int targetSum) {
+        if(root == nullptr) return false;
+        if(root->left == nullptr && root->right == nullptr){
+            if(targetSum - root->val == 0){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+
+        bool leftFlag = false, rightFlag = false;  
+        if(root->left) leftFlag = hasPathSum(root->left, targetSum - root->val);
+        if(root->right) rightFlag = hasPathSum(root->right, targetSum - root->val);
+        return leftFlag || rightFlag;
+    }
+};
+//比上面精简些
+class Solution {
+public:
+    bool hasPathSum(TreeNode* root, int targetSum) {
+        if(root == nullptr) return false;
+        if(root->left == nullptr && root->right == nullptr){
+            if(targetSum - root->val == 0){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+        return hasPathSum(root->left, targetSum - root->val) || hasPathSum(root->right, targetSum - root->val);
+    }
+};
