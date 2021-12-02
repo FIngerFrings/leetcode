ad800c89b21364868f319316288ce28def21cd7a
Finger Frings <67583493+FIngerFrings@users.noreply.github.com>
2021-12-02 20:36:10 +0800

    Update leetcode513.cpp

--- Easy/leetcode513.cpp
+++ Easy/leetcode513.cpp
@@ -2,76 +2,51 @@
  * 给定一个二叉树的 根节点 root，请找出该二叉树的 最底层 最左边 节点的值。
  * 假设二叉树中至少有一个节点。
  */
-//方法一：广度优先搜索
-//思路：使用广度优先搜索，先将右节点放入队列中，再将左结点放入，这样搜索的最后一个结点就是最底层最左边的结点
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+//方法一：层序遍历
+//思路：层序遍历，获取每一层的第一个数，遍历完后得到的就是最底层最左边节点的值
 class Solution {
 public:
     int findBottomLeftValue(TreeNode* root) {
-        TreeNode *node = nullptr;
-        queue<TreeNode *> q;
+        queue<TreeNode*> q;
         q.push(root);
+        int ans = root->val;
         while(!q.empty()){
             int n = q.size();
+            ans = q.front()->val;
             while(n--){
-                node = q.front();
+                auto node = q.front();
                 q.pop();
-                if(node->right)  q.push(node->right);
                 if(node->left) q.push(node->left);
+                if(node->right) q.push(node->right);
             }
         }
-        return node->val;
+        return ans;
     }
 };
 
-//实际上题目要求的是最底层最左边的结点，并不是说明它就是左结点，所以这里也有可能是右节点
-//因此只需要递归到最底层，同时先处理左结点即可
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+//方法二：递归
+//思路：实际上就是找到深度最大且最左边的节点，通过遍历过程中传入当前节点的深度，并判断当前节点是否是叶子节点，如果是则比较深度
+//找到其中深度最大的叶子节点，为了保证是最左边的，所以可以前序遍历
 class Solution {
 public:
+    int maxHeight;
     int ans;
-    int maxlen = -1;
 
-    void findBottomHelper(TreeNode *root, int level){
-        if(!root->left && !root->right){
-            if(level > maxlen){
+    void traversal(TreeNode* root, int height){
+        if(root->left == nullptr && root->right == nullptr){
+            if(height > maxHeight){
+                maxHeight = height;
                 ans = root->val;
-                maxlen = level;
             }
-            return;
         }
 
-        if(root->left){
-            findBottomHelper(root->left, level + 1);
-        }
-
-        if(root->right){
-            findBottomHelper(root->right, level + 1);
-        }
+        if(root->left) traversal(root->left, height + 1);
+        if(root->right) traversal(root->right, height + 1);
     }
 
     int findBottomLeftValue(TreeNode* root) {
-        findBottomHelper(root, 0);
+        maxHeight = INT_MIN;
+        traversal(root, 1);
         return ans;
     }
 };
