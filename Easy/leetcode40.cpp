ba5f1c27b04389a4a5526ea5e8f7b108946bbb62
Finger Frings <67583493+FIngerFrings@users.noreply.github.com>
2021-12-05 22:32:01 +0800

    Update leetcode40.cpp

--- Easy/leetcode40.cpp
+++ Easy/leetcode40.cpp
@@ -46,3 +46,110 @@ public:
         return ans;
     }
 };
+
+//回溯
+class Solution {
+public:
+    vector<vector<int>> ans;
+    vector<int> path;
+    vector<bool> vis;
+
+    void backtracking(vector<int> &candidates, int target, int sum, int cur){
+        if(sum > target)    return;
+        if(sum == target){
+            ans.push_back(path);
+            return;
+        }
+
+        for(int i = cur; i < candidates.size(); i++){
+            if(i > 0 && candidates[i] == candidates[i-1] && !vis[i-1]){
+                continue;
+            }
+            sum += candidates[i];
+            path.push_back(candidates[i]);
+            vis[i] = true;
+            backtracking(candidates, target, sum, i + 1);
+            sum -= candidates[i];
+            path.pop_back();
+            vis[i] = false;
+        }
+        return;
+    }
+
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        sort(candidates.begin(), candidates.end());
+        vis.resize(candidates.size());
+        backtracking(candidates, target, 0, 0);
+        return ans;    
+    }
+};
+
+//回溯+剪枝
+class Solution {
+public:
+    vector<vector<int>> ans;
+    vector<int> path;
+    vector<bool> vis;
+
+    void backtracking(vector<int> &candidates, int target, int sum, int cur){
+        if(sum > target)    return;
+        if(sum == target){
+            ans.push_back(path);
+            return;
+        }
+
+        for(int i = cur; i < candidates.size() && sum + candidates[i] <= target; i++){
+            if(i > 0 && candidates[i] == candidates[i-1] && !vis[i-1]){
+                continue;
+            }
+            sum += candidates[i];
+            path.push_back(candidates[i]);
+            vis[i] = true;
+            backtracking(candidates, target, sum, i + 1);
+            sum -= candidates[i];
+            path.pop_back();
+            vis[i] = false;
+        }
+        return;
+    }
+
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        sort(candidates.begin(), candidates.end());
+        vis.resize(candidates.size());
+        backtracking(candidates, target, 0, 0);
+        return ans;    
+    }
+};
+
+//回溯+剪枝+不需要vis，直接通过cur判断是否重复
+class Solution {
+public:
+    vector<vector<int>> ans;
+    vector<int> path;
+
+    void backtracking(vector<int> &candidates, int target, int sum, int cur){
+        if(sum > target)    return;
+        if(sum == target){
+            ans.push_back(path);
+            return;
+        }
+
+        for(int i = cur; i < candidates.size() && sum + candidates[i] <= target; i++){
+            if(i > cur && candidates[i] == candidates[i-1]){
+                continue;
+            }
+            sum += candidates[i];
+            path.push_back(candidates[i]);
+            backtracking(candidates, target, sum, i + 1);
+            sum -= candidates[i];
+            path.pop_back();
+        }
+        return;
+    }
+
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        sort(candidates.begin(), candidates.end());
+        backtracking(candidates, target, 0, 0);
+        return ans;    
+    }
+};
