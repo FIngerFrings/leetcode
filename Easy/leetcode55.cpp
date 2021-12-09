c172d335c0fe0d5a7c174e8e421d1212db7ddff2
Finger Frings <67583493+FIngerFrings@users.noreply.github.com>
2021-12-09 21:36:37 +0800

    Update leetcode55.cpp

--- Easy/leetcode55.cpp
+++ Easy/leetcode55.cpp
@@ -24,3 +24,17 @@ public:
         return false;
     }
 };
+
+//贪心
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int maxPos = 0;
+        for(int i = 0; i < nums.size(); i++){
+            if(i > maxPos)  return false;
+            maxPos = max(maxPos, i + nums[i]);
+            if(maxPos >= nums.size() - 1)   return true;
+        }
+        return true;
+    }
+};
