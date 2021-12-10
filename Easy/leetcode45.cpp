cc3846c82eeec8ef9d073cadc6a8ed25c5547083
Finger Frings <67583493+FIngerFrings@users.noreply.github.com>
2021-12-10 10:55:51 +0800

    Update leetcode45.cpp

--- Easy/leetcode45.cpp
+++ Easy/leetcode45.cpp
@@ -31,7 +31,7 @@ public:
 };
 
 //方法二：代码随想录，回溯
-//思路：思想就一句话：每次在上次能跳到的范围（end）内选择一个能跳的最远的位置（也就是能跳到max_far位置的点）作为下次的起跳点 ！
+//思路：思想就一句话：每次在上次能跳到的范围（end）内选择一个能跳的最远的位置（也就是能跳到max_far位置的点）作为下次的起跳点
 class Solution {
 public:
     int jump(vector<int>& nums) {
@@ -47,6 +47,49 @@ public:
                     if(nextDistance >= nums.size() - 1) break;
                 }
                 else break;
+            }
+        }
+        return ans;
+    }
+};
+
+//其实代码随想录贪心方法一里面不需要判断curDistance是否是在最后一个位置，因为能在这种情况只有数组大小为1时才可能出现，否则都会在上一次更新时直接退出
+//此时如果没有判断数组大小是否等于1，会出错
+//所有if(nums.size() == 1)    return 0;和if(curDistance != nums.size() - 1)只用一个就行
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        if(nums.size() == 1)    return 0;
+        int ans = 0;
+        int curDistance = 0;
+        int nextDistance = 0;
+        for(int i = 0; i < nums.size(); i++){
+            nextDistance = max(nums[i] + i, nextDistance);
+            if(i == curDistance){
+                ++ans;
+                curDistance = nextDistance;
+                if(nextDistance >= nums.size() - 1) break;
+            }
+        }
+        return ans;
+    }
+};
+
+//贪心方法二
+//其实思路还是和方法一相同，这里for循环只到nums.size() - 1是因为，防止出现curDistance刚好是最后一个元素，然后又多算了一次
+//如果for循环到最后一个元素，则如果说curDistance刚好到达最后一个位置，则结果会多1
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        if(nums.size() == 1)    return 0;
+        int ans = 0;
+        int curDistance = 0;
+        int nextDistance = 0;
+        for(int i = 0; i < nums.size() - 1; i++){
+            nextDistance = max(nums[i] + i, nextDistance);
+            if(i == curDistance){
+                curDistance = nextDistance;
+                ++ans;
             }
         }
         return ans;
