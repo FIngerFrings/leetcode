ceae08d3a1db1eab0f90f3d69133910ffab8bacb
Finger Frings <67583493+FIngerFrings@users.noreply.github.com>
2021-12-12 22:00:03 +0800

    Update leetcode62.cpp

--- Easy/leetcode62.cpp
+++ Easy/leetcode62.cpp
@@ -27,6 +27,25 @@ public:
     }
 };
 
+//动态规划
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(i == 0 || j == 0){
+                    dp[i][j] = 1;
+                }
+                else{
+                    dp[i][j] = dp[i-1][j] + dp[i][j-1];
+                }
+            }
+        }
+        return dp[m-1][n-1];
+    }
+};
+
 //方法二：组合数字
 //思路:从左上角到右下角的过程中，我们需要移动 m+n-2m+n−2 次，其中有 m-1m−1 次向下移动，n-1n−1 次向右移动。因此路径的总数，
 //就等于从 m+n-2m+n−2 次移动中选择 m-1m−1 次向下移动的方案数，即组合数：C(m-1)(m+n-1).具体公式看解答
