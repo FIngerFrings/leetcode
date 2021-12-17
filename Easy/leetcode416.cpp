0421783880d0f78dd67e7ca65120fa4416c58217
Finger Frings <67583493+FIngerFrings@users.noreply.github.com>
2021-12-17 21:21:49 +0800

    Update leetcode416.cpp

--- Easy/leetcode416.cpp
+++ Easy/leetcode416.cpp
@@ -131,3 +131,27 @@ public:
         return false;
     }
 };
+
+//动态规划
+class Solution {
+public:
+    bool canPartition(vector<int>& nums) {
+        int sum = 0;
+        for(int num : nums) sum += num;
+        if(sum % 2 != 0) return false;
+        sum /= 2;
+        int n = nums.size();
+        vector<vector<int>> dp(n+1, vector<int>(sum+1, 0));
+        for(int i = 1; i <= n; i++){
+            for(int j = 0; j <= sum; j++){
+                if(j >= nums[i-1]){
+                    dp[i][j] = max(dp[i-1][j], dp[i-1][j-nums[i-1]] + nums[i-1]);
+                }
+                else{
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+        return dp[n][sum] == sum;
+    }
+};
