4a6371785fc8c1db1895ed1da54388b994461db7
Finger Frings <67583493+FIngerFrings@users.noreply.github.com>
2021-12-18 20:15:21 +0800

    Update leetcode377.cpp

--- Easy/leetcode377.cpp
+++ Easy/leetcode377.cpp
@@ -3,3 +3,40 @@
  * 题目数据保证答案符合 32 位整数范围。
  */
 //方法一：动态规划
+class Solution {
+public:
+    int combinationSum4(vector<int>& nums, int target) {
+        int n = nums.size();
+        vector<vector<int>> dp(n+1, vector<int>(target+1, 0));
+        dp[0][0] = 1;
+        for(int j = 0; j <= target; j++){
+            for(int i = 1; i <= n; i++){
+                if(j >= nums[i-1] && dp[i-1][j] < INT_MAX - dp[n][j-nums[i-1]]){
+                   dp[i][j] = dp[i-1][j] + dp[n][j-nums[i-1]]; 
+                }
+                else{
+                    dp[i][j] = dp[i-1][j]; 
+                }
+            }
+        }
+        return dp[n][target];
+    }
+};
+
+//压缩空间的动态规划
+class Solution {
+public:
+    int combinationSum4(vector<int>& nums, int target) {
+        int n = nums.size();
+        vector<int> dp(target+1, 0);
+        dp[0] = 1;
+        for(int j = 0; j <= target; j++){
+            for(int i = 1; i <= n; i++){
+                if(j >= nums[i-1] && dp[j - nums[i-1]] < INT_MAX - dp[j]){
+                    dp[j] += dp[j-nums[i-1]];
+                }
+            }
+        }
+        return dp[target];
+    }
+};
