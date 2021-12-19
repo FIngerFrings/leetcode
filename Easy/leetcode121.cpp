a2d9718222425dd347d09dce4560ad5a42392a4b
Finger Frings <67583493+FIngerFrings@users.noreply.github.com>
2021-12-19 16:04:18 +0800

    Update leetcode121.cpp

--- Easy/leetcode121.cpp
+++ Easy/leetcode121.cpp
@@ -69,3 +69,18 @@ public:
         return maxp;
     }
 };
+
+//方法二：动态规划
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        vector<vector<int>> dp(n, vector<int>(2, 0));
+        dp[0][0] = -prices[0];
+        for(int i = 1; i < n; i++){
+            dp[i][0] = max(dp[i-1][0], -prices[i]);
+            dp[i][1] = max(dp[i-1][1], dp[i-1][0] + prices[i]);
+        }
+        return max(dp[n-1][0], dp[n-1][1]);
+    }
+};
