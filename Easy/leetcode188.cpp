3548bf8973dced48e747226e125f05940901bd94
Finger Frings <67583493+FIngerFrings@users.noreply.github.com>
2021-12-21 16:32:47 +0800

    Update leetcode188.cpp

--- Easy/leetcode188.cpp
+++ Easy/leetcode188.cpp
@@ -31,3 +31,29 @@ public:
         return *max_element(sell[n-1].begin(), sell[n-1].end());
     }
 };
+
+//代码随想录动态规划
+//思路：实际上就是之前股票问题动态规划的加强版
+class Solution {
+public:
+    int maxProfit(int k, vector<int>& prices) {
+        int n = prices.size();
+        if(k == 0 || n == 0) return 0;
+        vector<vector<int>> dp(n, vector<int>(2*k+1, 0));
+        for(int i = 1; i < 2*k+1; i+=2){
+            dp[0][i] = -prices[0];
+        }
+        for(int i = 1; i < n; i++){
+            dp[i][0] = dp[i-1][0];
+            for(int j = 1; j < 2*k+1; j++){
+                if(j % 2 != 0){
+                    dp[i][j] = max(dp[i-1][j], dp[i-1][j-1] - prices[i]);
+                }
+                else{
+                    dp[i][j] = max(dp[i-1][j], dp[i-1][j-1] + prices[i]);
+                }
+            }
+        }
+        return dp[n-1][2*k];
+    }
+};
