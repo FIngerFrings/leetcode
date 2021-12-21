2bc0319dfacd356480ce12ff25be6139055e6ae7
Finger Frings <67583493+FIngerFrings@users.noreply.github.com>
2021-12-21 17:32:21 +0800

    Update leetcode309.cpp

--- Easy/leetcode309.cpp
+++ Easy/leetcode309.cpp
@@ -21,3 +21,38 @@ public:
         return max(dp[n-1][1], dp[n-1][2]);
     }
 };
+
+//自己写的动态规划
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        if(n == 1) return 0;
+        vector<vector<int>> dp(n, vector<int>(2, 0));
+        dp[0][0] = -prices[0];
+        dp[1][0] = max(dp[0][0], -prices[1]);
+        dp[1][1] = max(dp[0][1], dp[0][0] + prices[1]);
+        for(int i = 2; i < n; i++){
+            dp[i][0] = max(dp[i-1][0], dp[i-2][1] - prices[i]);
+            dp[i][1] = max(dp[i-1][1], dp[i-1][0] + prices[i]);
+        }
+        return max(dp[n-1][0], dp[n-1][1]);
+    }
+};
+
+//代码随想录动态规划，4个状态
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        vector<vector<int>> dp(n, vector<int>(4, 0));
+        dp[0][0] = -prices[0];
+        for(int i = 1; i < n; i++){
+            dp[i][0] = max(dp[i-1][0], max(dp[i-1][3], dp[i-1][1]) - prices[i]);
+            dp[i][1] = max(dp[i-1][1], dp[i-1][3]);
+            dp[i][2] = dp[i-1][0] + prices[i];
+            dp[i][3] = dp[i-1][2];
+        }
+        return max(dp[n-1][3], max(dp[n-1][1], dp[n-1][2]));
+    }
+};
