9b94c012e546a483a902cebedb80edb520413671
Finger Frings <67583493+FIngerFrings@users.noreply.github.com>
2021-12-22 11:18:36 +0800

    Update leetcode647.cpp

--- Easy/leetcode647.cpp
+++ Easy/leetcode647.cpp
@@ -106,3 +106,28 @@ public:
         return ans;
     }
 };
+
+//方法三：动态规划
+class Solution {
+public:
+    int countSubstrings(string s) {
+        int n = s.size();
+        vector<vector<bool>> dp(n, vector<bool>(n, false));
+        int ans = 0;
+        for(int i = n-1; i >= 0; i--){
+            for(int j = i; j < n; j++){
+                if(s[j] == s[i]){
+                    if(j - i <= 1){
+                        ++ans;
+                        dp[i][j] = true;
+                    }
+                    else if(dp[i+1][j-1]){
+                        ++ans;
+                        dp[i][j] = true;
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+};
