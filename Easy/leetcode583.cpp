c152b3649f2f70982e26b8baf669f179409a2a86
Finger Frings <67583493+FIngerFrings@users.noreply.github.com>
2021-12-22 10:14:23 +0800

    Update leetcode583.cpp

--- Easy/leetcode583.cpp
+++ Easy/leetcode583.cpp
@@ -89,6 +89,25 @@ public:
     }
 };
 
+//后来自己写的动态规划
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        int m = word1.size(), n = word2.size();
+        vector<vector<int>> dp(m+1, vector<int>(n+1, 0));
+        for(int i = 0; i <= m; i++){
+            for(int j = 0; j <= n; j++){
+                if(i == 0 && j == 0) continue;
+                if(i == 0) dp[i][j] = j;
+                else if(j == 0) dp[i][j] = i;
+                else if(word1[i-1] == word2[j-1]) dp[i][j] = dp[i-1][j-1];
+                else dp[i][j] = min(dp[i-1][j], dp[i][j-1]) + 1;
+            }
+        }
+        return dp[m][n];
+    }
+};
+
 //方法二：最长公共子序列
 //思路：我们可以求出word1和word2的最长公共子序列的长度，最小步数就是word1.size() + word2.size() - 2 * 最小公共子序列长度
 //这里是使用了动态规划求的最长公共子序列
