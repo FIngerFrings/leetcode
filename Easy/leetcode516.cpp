b8eabe897f7703c0276ee8307c2362f7de0b43a3
Finger Frings <67583493+FIngerFrings@users.noreply.github.com>
2021-12-22 15:56:25 +0800

    Create leetcode516.cpp

---
+++ Easy/leetcode516.cpp
@@ -0,0 +1,20 @@
+/*
+ * 给你一个字符串 s ，找出其中最长的回文子序列，并返回该序列的长度。
+ * 子序列定义为：不改变剩余字符顺序的情况下，删除某些字符或者不删除任何字符形成的一个序列。
+ */
+//方法一：动态规划
+class Solution {
+public:
+    int longestPalindromeSubseq(string s) {
+        int n = s.size();
+        vector<vector<int>> dp(n, vector<int>(n, 0));
+        for(int i = 0; i < n; i++) dp[i][i] = 1;
+        for(int i = n-1; i >= 0; i--){
+            for(int j = i+1; j < n; j++){
+                if(s[i] == s[j]) dp[i][j] = dp[i+1][j-1] + 2;
+                else dp[i][j] = max(dp[i+1][j], dp[i][j-1]);
+            }
+        }
+        return dp[0][n-1];
+    }
+};
