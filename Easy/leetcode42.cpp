e4e33daecbc62fd93d70884f10da1ccb11aef5b1
Finger Frings <67583493+FIngerFrings@users.noreply.github.com>
2021-12-22 19:53:08 +0800

    Update leetcode42.cpp

--- Easy/leetcode42.cpp
+++ Easy/leetcode42.cpp
@@ -28,6 +28,30 @@ public:
     }
 };
 
+//代码随想录动态规划
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int ans = 0;
+        int n = height.size();
+        vector<int> maxLeft(n, 0);
+        vector<int> maxRight(n, 0);
+        maxLeft[0] = height[0];
+        maxRight[n-1] = height[n-1];
+        for(int i = 1; i < n; i++){
+            maxLeft[i] = max(height[i], maxLeft[i-1]);
+        }
+        for(int i = n-2; i >= 0; i--){
+            maxRight[i] = max(height[i], maxRight[i+1]);
+        }
+        for(int i = 0; i < n; i++){
+            int h = min(maxLeft[i], maxRight[i]) - height[i];
+            if(h > 0) ans += h;
+        }
+        return ans;
+    }
+};
+
 //方法二：单调栈
 //思路：emmmmm其实我还并不是特别理解这道题单调栈的解法，可以取官方解答看看
 class Solution {
@@ -47,6 +71,64 @@ public:
                 int curr_width = i - left - 1;
                 int curr_height = min(height[left], height[i]) - height[k];
                 ans += curr_width * curr_height;
+            }
+            stk.push(i);
+        }
+        return ans;
+    }
+};
+
+//代码随想录单调栈
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        if(height.size() <= 2) return 0;
+        int n = height.size();
+        stack<int> stk;
+        stk.push(0);
+        int ans = 0;
+        for(int i = 1; i < n; i++){
+            if(!stk.empty() && height[i] < height[stk.top()]){
+                stk.push(i);
+            }
+            else if(!stk.empty() && height[i] == height[stk.top()]){
+                stk.pop();
+                stk.push(i);
+            }
+            else{
+                while(!stk.empty() && height[i] > height[stk.top()]){
+                    int mid = stk.top();
+                    stk.pop();
+                    if(!stk.empty()){
+                        int h = min(height[i], height[stk.top()]) - height[mid];
+                        int w = i - stk.top() - 1;
+                        ans += h * w;
+                    }
+                }
+                stk.push(i);
+            }
+        }
+        return ans;
+    }
+};
+
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        if(height.size() <= 2) return 0;
+        int n = height.size();
+        stack<int> stk;
+        stk.push(0);
+        int ans = 0;
+        for(int i = 1; i < n; i++){
+            while(!stk.empty() && height[i] > height[stk.top()]){
+                int mid = stk.top();
+                stk.pop();
+                if(!stk.empty()){
+                    int h = min(height[i], height[stk.top()]) - height[mid];
+                    int w = i - stk.top() - 1;
+                    ans += h * w;
+                }
             }
             stk.push(i);
         }
@@ -74,6 +156,31 @@ public:
                 ans += rightmax - height[right];
                 right--;
             }
+        }
+        return ans;
+    }
+};
+
+//双指针，超时
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int ans = 0;
+        int n = height.size();
+        for(int i = 0; i < n; i++){
+            if(i == 0 || i == n-1) continue;
+            int rHeight = height[i];
+            int lHeight = height[i];
+            for(int r = i + 1; r < n; r++){
+                if(height[r] > rHeight) rHeight = height[r];
+            }
+
+            for(int l = i - 1; l >= 0; l--){
+                if(height[l] > lHeight) lHeight = height[l];
+            }
+
+            int h = min(lHeight, rHeight) - height[i];
+            if(h > 0) ans += h;
         }
         return ans;
     }
