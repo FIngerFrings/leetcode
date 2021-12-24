aae36a761d362e132d00338ef005f4c5cb965b7e
Finger Frings <67583493+FIngerFrings@users.noreply.github.com>
2021-12-24 10:26:20 +0800

    Update leetcode69.cpp

--- Easy/leetcode69.cpp
+++ Easy/leetcode69.cpp
@@ -99,3 +99,24 @@ public:
         return (int)x0;
     }
 };
+
+//二分查找
+class Solution {
+public:
+    int mySqrt(int x) {
+        int l = 0, r = x;
+        while(l <= r){
+            long long mid = l + (r - l) / 2;
+            if(mid * mid == x){
+                return mid;
+            }
+            else if(mid * mid > x){
+                r = mid - 1;
+            }
+            else{
+                l = mid + 1;
+            }
+        }
+        return r;
+    }
+};
